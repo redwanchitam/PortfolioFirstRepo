00f6a899f54eecc5b8cfcad20c1b1b3e857f5918
redwanchitam <der1goiada@gmail.com>
2020-12-16 16:25:10 +0100

    whoAmI content

--- resources/js/components/WhoAmIComponent.vue
+++ resources/js/components/WhoAmIComponent.vue
@@ -1,10 +1,154 @@
 <template>
-    <div class="">
-        <div class="row justify-content-center align-items-center coverPage">
-            <div class="ml-auto col-lg-5 col-md-5 col-sm-12">
-                <div class="row align-items-center">
-                    <h1>i'm really glad that you arrived here ..</h1>
-                    <p>here will be some infos about me ...</p>
+    <div class="container">
+        <div class="pt-5 row">
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="">
+                    <h2 class="sectionTitle mb-4">About Me :</h2>
+                    <div class="ml-2 ">
+                        <p>i'm Redwan, a fullstack web developer, with an innate attraction for therms of art and simplicity.
+                        <br>i'm interested in building responsive apps and design illustrations</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="">
+                    <h2 class="sectionTitle mb-4">Skills :</h2>
+                    <div class="d-flex">
+                        <div class="ml-2 w-100 row justify-content-around">
+                            <div class="px-0 col-lg-6 col-md-6 col-sm-12">
+                                <div class="leftLine  ">
+                                    <h4 class="sectionSubTitle">FrontEnd</h4>
+                                    <div>
+                                        <ul class="list-group list-group-flush tagList skillsList">
+                                            <li class="skillsItem ">
+                                                <div class="d-flex m-0">
+                                                    <h5>html</h5>
+                                                </div>
+                                            </li>
+                                            <li class="skillsItem ">
+                                                <div class="d-flex m-0">
+                                                    <h5>css</h5>
+                                                </div>
+                                            </li>
+                                            <li class="skillsItem ">
+                                                <div class="d-flex m-0">
+                                                    <h5>js/jquery</h5>
+                                                </div>
+                                            </li>
+                                            <li class="skillsItem ">
+                                                <div class="d-flex m-0">
+                                                    <h5>VueJs</h5>
+                                                </div>
+                                            </li><li class="skillsItem ">
+                                                <div class="d-flex m-0">
+                                                    <h5>Bootstarp</h5>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="px-0 col-lg-6 col-md-6 col-sm-12">
+                                <div class="leftLine  ">
+                                    <h4 class="sectionSubTitle">backEnd</h4>
+                                    <div>
+                                        <ul class="list-group list-group-flush tagList skillsList">
+                                            <li class="skillsItem ">
+                                                <div class="d-flex m-0">
+                                                    <h5>.NET framework</h5>
+                                                </div>
+                                            </li>
+                                            <li class="skillsItem ">
+                                                <div class="d-flex m-0">
+                                                    <h5>php ( laravel )</h5>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pr-0 col-lg-6 col-md-6 col-sm-12">
+                                <div class="leftLine  ">
+                                    <h4 class="sectionSubTitle">Design</h4>
+                                    <div>
+                                        <ul class="list-group list-group-flush tagList skillsList">
+                                            <li class="skillsItem ">
+                                                <div class="d-flex m-0">
+                                                    <h5>Adobe Illustrator</h5>
+                                                </div>
+                                            </li>
+                                            <li class="skillsItem">
+                                                <div class="d-flex m-0">
+                                                    <h5>Adobe Xd</h5>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-12 col-sm-12">
+                <div class="">
+                    <h2 class="sectionTitle mb-4">Education and diplomats :</h2>
+                    <div class="ml-2">
+                        <ul class="list-group list-group-flush tagList">
+                            <li class="diplomatsItem leftLine  ">
+                                <div class="d-flex m-0">
+                                    <h5 class="sectionSubTitle">2020</h5>
+                                </div>
+                                <div class="">
+                                    <div class="d-flex m-0">
+                                        <h5>Bachlor in economic and legal siences</h5>
+                                    </div>
+                                    <div class="d-flex m-0">
+                                        <h6>Ibn zohr university - Morocco</h6>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="diplomatsItem leftLine  ">
+                                <div class="d-flex m-0">
+                                    <h5 class="sectionSubTitle">2019</h5>
+                                </div>
+                                <div class="">
+                                    <div class="d-flex m-0">
+                                        <h5>Associate's degree in development</h5>
+                                    </div>
+                                    <div class="d-flex m-0">
+                                        <h6>Higher Institute of Applied Technologies - Morocco</h6>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="diplomatsItem leftLine  ">
+                                <div class="d-flex m-0">
+                                    <h5 class="sectionSubTitle">2017</h5>
+                                </div>
+                                <div class="">
+                                    <div class="d-flex m-0">
+                                        <h5>Technician's certificate in informatique maintenance and networking</h5>
+                                    </div>
+                                    <div class="d-flex m-0">
+                                        <h6>Higher Institute of Applied Technologies - Morocco</h6>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="diplomatsItem leftLine  ">
+                                <div class="d-flex m-0">
+                                    <h5 class="sectionSubTitle">2015</h5>
+                                </div>
+                                <div class="">
+                                    <div class="d-flex m-0">
+                                        <h5>Baccalaureate in phisics siences</h5>
+                                    </div>
+                                    <div class="d-flex m-0">
+                                        <h6>Hassan the 2nd, high school- Morocco</h6>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
